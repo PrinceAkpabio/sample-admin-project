c958914afad3424f78a8b2c43a78e9a90a81cf6b
Prince Akpabio <princeakpabio90@gmail.com>
2024-02-01 18:06:21 +0100

    Fix: corrected guard path in app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './Guard/auth/auth.guard';
+import { AuthGuard } from './guard/auth/auth.guard';
 
 const routes: Routes = [
   {
